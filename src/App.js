64bbe4da55f49ca74134038a53adec36215e7741
Mohit Punia <mohitpunia@innotechteam.in>
2023-04-08 12:07:36 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import './App.css';
-import tasks from './tasks.json'
-import { useState } from "react"
+import "./App.css";
+import tasks from "./tasks.json";
+import { useState } from "react";
 
 function App() {
-
-  const [indexCheck, setIndexCheck] = useState([])
+  const [indexCheck, setIndexCheck] = useState([]);
 
   let handleClick = (event, index) => {
-    const { checked } = event.target
+    const { checked } = event.target;
     if (checked === true) {
-      setIndexCheck(oldArray => [...oldArray, index])
+      setIndexCheck((oldArray) => [...oldArray, index]);
+    } else {
+      setIndexCheck(indexCheck.filter((item) => item !== index));
     }
-    else {
-      setIndexCheck(indexCheck.filter(item => item !== index))
-    }
-  }
+  };
 
-  return (<div className='container'>
-    <h3>Todo App</h3>
-    {tasks.map((element, index) => {
-      return <>
-        <div className='list' key={index}>
-          <div>
-            <input type="checkbox" id={index} name={index} value={element.task} onClick={(event) => { handleClick(event, index) }} key={index} />
-            <label for={index}>{element.task}{index}</label>
-          </div>
-          <div>{
-            indexCheck.includes(index) ? <button variant="outline-secondary">Complete</button> : <></>}</div>
-        </div>
-      </>
-    })}
-  </div>
+  return (
+    <div className="container">
+      <h3>Todo App</h3>
+      {tasks.map((element, index) => {
+        return (
+          <>
+            <div className="list" key={index}>
+              <div>
+                <input
+                  type="checkbox"
+                  id={index}
+                  name={index}
+                  value={element.task}
+                  onClick={(event) => {
+                    handleClick(event, index);
+                  }}
+                  key={index}
+                />
+                <label for={index}>
+                  {element.task}
+                  {index}
+                </label>
+              </div>
+              <div>
+                {indexCheck.includes(index) ? (
+                  <button variant="outline-secondary">Complete</button>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
   );
 }
 
