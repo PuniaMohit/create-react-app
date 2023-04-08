83f0bc4a8a566cba51548aaa03e93517104f85c2
Mohit Punia <mohitpunia@innotechteam.in>
2023-04-08 11:40:30 +0530

    changes suggested by sir

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import './App.css';
-import Tasks from './todo.json'
+import tasks from './tasks.json'
 import { useState } from "react"
 
 function App() {
 
   const [indexCheck, setIndexCheck] = useState([])
 
-  function change(event, index) {
+  let handleClick = (event, index) => {
     const { checked } = event.target
-  if(checked===true){
-    setIndexCheck(oldArray => [...oldArray, index])
-  }
-  else{
-    setIndexCheck(indexCheck.filter(item => item !== index))
-  }
+    if (checked === true) {
+      setIndexCheck(oldArray => [...oldArray, index])
+    }
+    else {
+      setIndexCheck(indexCheck.filter(item => item !== index))
+    }
   }
 
   return (<div className='container'>
     <h3>Todo App</h3>
-      {Tasks.map((element, index) => {
-        return <>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }} key={index}>
-            <div>
-              <input type="checkbox" id={index} name={index} value={element.task} onClick={(event) => { change(event, index) }} key={index}/>
-              <label for={index}>{element.task}{index}</label>
-            </div>
-            <div>{ 
-            indexCheck.includes(index) ? <button variant="outline-secondary">Complete</button> : <></>}</div>
+    {tasks.map((element, index) => {
+      return <>
+        <div className='list' key={index}>
+          <div>
+            <input type="checkbox" id={index} name={index} value={element.task} onClick={(event) => { handleClick(event, index) }} key={index} />
+            <label for={index}>{element.task}{index}</label>
           </div>
-          <hr />
-        </>
-      })}
+          <div>{
+            indexCheck.includes(index) ? <button variant="outline-secondary">Complete</button> : <></>}</div>
+        </div>
+      </>
+    })}
   </div>
   );
 }
